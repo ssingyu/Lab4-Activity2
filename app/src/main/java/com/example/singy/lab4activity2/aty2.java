6f9a5b59174f2c10c46c372c96fa68336bfa6f3d
ssingyu <singyu@outlook.com>
2017-10-18 04:34:31 +0800

    Added aty2

---
+++ app/src/main/java/com/example/singy/lab4activity2/aty2.java
@@ -0,0 +1,13 @@
+package com.example.singy.lab4activity2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class aty2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main2);
+    }
+}
